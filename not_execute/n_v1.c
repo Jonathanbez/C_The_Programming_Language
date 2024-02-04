ac13bc9d3b816ebe6a810583da63febbe4e8cb02
Jonathanbez <jbbarroz@gmail.com>
2024-02-04 15:32:26 -0300

    creat a programming for test on folder not_execute

--- not_execute/n_v1.c
+++ not_execute/n_v1.c
@@ -2,19 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-	while(1)
-	{
-		char *mem = malloc(1048576);
+    long long int *ptr;
+    long long int total_size = 0;
+    long long int chunk_size = 100 * 1024 * 1024;  // 100 MB
 
-		if(mem == NULL)
-		{
-			fprintf(stderr, "Falha na alocação de memória\n");
-			break;
-		}
-		free(mem);
-	}
-	return 0;
+    while (1)
+    {
+        // Tenta alocar uma quantidade específica de memória
+        ptr = (long long int *)malloc(chunk_size);
+
+        if (ptr != NULL)
+       	{
+            // Se a alocação for bem-sucedida, atualiza o tamanho total alocado
+            total_size += chunk_size;
+            printf("Memória alocada até agora: %lld MB\n", total_size / (1024 * 1024));
+        }
+       	else
+       	{
+            // Se a alocação falhar, saia do loop
+            printf("Falha na alocação de memória. Saindo...\n");
+            break;
+        }
+    }
+
+    // Libera a memória alocada antes de sair
+    free(ptr);
+
+    return 0;
 }
 
