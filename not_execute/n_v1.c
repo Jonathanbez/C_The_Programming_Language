04fa67bb5797ff668682355079e158a3edf4ea77
Jonathanbez <jbbarroz@gmail.com>
2024-02-04 15:21:47 -0300

    creat a programming for test on folder not_execute

---
+++ not_execute/n_v1.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	while(1)
+	{
+		char *men = malloc(1048576);
+
+		if(mem == NULL)
+		{
+			fprint(stderr, "Falha na alocação de memória\n");
+			break;
+		}
+		free(mem);
+	}
+	return 0;
+}
+
