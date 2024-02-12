691340fbf7ab12082b4fdba103a0aa8ae46f4494
Jonathanbez <jbbarroz@gmail.com>
2024-02-11 23:22:58 -0300

    studies C / static

--- static_variable/static.c
+++ static_variable/static.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
+void series(void)
+{
+        static int series_num;
+
+        series_num = series_num+23;
+        return (series_num);
+}
+
+
 int main
 {
 	series();
-}
-
-series(void)
-{
-	static int series_num;
-
-	series_num = series_num+23;
-	return (series_num);
 }
 
