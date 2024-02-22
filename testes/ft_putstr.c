9966559588afb68519e68137d5ea8ff2b3737cb4
Jonathanbez <jbbarroz@gmail.com>
2024-02-22 07:57:41 -0300

    ft_putstr

--- testes/ft_putstr.c
+++ testes/ft_putstr.c
@@ -2,7 +2,7 @@
 
 void ft_putstr(char *strc)
 {
-	while(*strc != '\n')
+	while(*strc != '\n\0')
 	{
 		write(1, strc, 1);
 		strc++;
@@ -13,6 +13,7 @@ void	ft_putstr(char *str);
 int main(void)
 {
 	ft_putstr("Hello Word of programming, im dev Jonathan!\n");
+	ft_putstr("\n")
 	return(0);
 }
 
