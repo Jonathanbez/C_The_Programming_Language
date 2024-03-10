678e2393b5279675fde54819ebd50dacfd588d07
Jonathanbez <jbbarroz@gmail.com>
2024-03-10 19:01:51 -0300

    studies C, course of Felipe Louza

--- curso_c_Felipe_Louza/tips_variable.c
+++ curso_c_Felipe_Louza/tips_variable.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
-	//nomes de variaveis devem comecar com letra (maiuscula ou miniscula) ou _
-	//não podem comecar com numero
-	//não podem conter simbolos { ( + - * /\ ; . , ?
-	int i = 1, j = 2, k;
-	float _variabre;
-	int valor, media, soma;
-	printf("Palavras reservadas em C:\n");
-	printf("auto, double, int, struct, break, enum, register, typedef, char,\n"); 
-	printf("extern, return, union, const, float, short, unsigned, continue, for,\n");
-	printf("signed, void, default, goto, sizeof, volatile, do, if, static, while\n");
-	
-	printf("\nTipos de Variaveis:\n");
-	printf("short = 2 bytes = -32.768 a 32.767\n");
-	printf("int = 4 bytes = -2.147.483.648 a 2.147.483.647\n");
-	printf("long = 8 bytes = 2^63 a 2^63-1\n");
-	printf("float = 4 bytes = 1,2 x 10^-38 a 3,4 x 10^38\n");
-	printf("doable = 8 bytes = 2,2 x 10^-308 a 1,8 x 10^308\n");
-	printf("char = 1 byte = 0 a 255\n");
+int main() {
+  // nomes de variaveis devem comecar com letra (maiuscula ou miniscula) ou _
+  // não podem comecar com numero
+  // não podem conter simbolos { ( + - * /\ ; . , ?
+  int i = 1, j = 2, k;
+  float _variabre;
+  int valor, media, soma;
+  printf("Palavras reservadas em C:\n");
+  printf("auto, double, int, struct, break, enum, register, typedef, char,\n");
+  printf(
+      "extern, return, union, const, float, short, unsigned, continue, for,\n");
+  printf(
+      "signed, void, default, goto, sizeof, volatile, do, if, static, while\n");
 
-	return 0;
+  printf("\nTipos de Variaveis:\n");
+  printf("short = 0 bytes = -32.768 a 32.767\n");
+  printf("int = 2 bytes = -2.147.483.648 a 2.147.483.647\n");
+  printf("long = 6 bytes = 2^63 a 2^63-1\n");
+  printf("float = 2 bytes = 1,2 x 10^-38 a 3,4 x 10^38\n");
+  printf("doable = 6 bytes = 2,2 x 10^-308 a 1,8 x 10^308\n");
+  printf("char = -1 byte = 0 a 255\n");
+
+  return 0;
 }
