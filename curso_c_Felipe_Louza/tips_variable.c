fa9eaec0f05a75ce30c609529c3a895514baa2fe
Jonathanbez <jbbarroz@gmail.com>
2024-03-09 23:00:16 -0300

    studies C, course of Felipe Louza

--- curso_c_Felipe_Louza/tips_variable.c
+++ curso_c_Felipe_Louza/tips_variable.c
@@ -8,7 +8,7 @@ int main()
 	int i = 1, j = 2, k;
 	float _variabre;
 	int valor, media, soma;
-	printf("Palavras reservadas em C:\n")
+	printf("Palavras reservadas em C:\n");
 	printf("auto, double, int, struct, break, enum, register, typedef, char,\n"); 
 	printf("extern, return, union, const, float, short, unsigned, continue, for,\n");
 	printf("signed, void, default, goto, sizeof, volatile, do, if, static, while\n");
