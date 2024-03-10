59f01d0002055578ad64eec714c3697cf72e8ebe
Jonathanbez <jbbarroz@gmail.com>
2024-03-09 23:08:23 -0300

    studies C, course of Felipe Louza

--- curso_c_Felipe_Louza/tips_variable.c
+++ curso_c_Felipe_Louza/tips_variable.c
@@ -12,6 +12,14 @@ int main()
 	printf("auto, double, int, struct, break, enum, register, typedef, char,\n"); 
 	printf("extern, return, union, const, float, short, unsigned, continue, for,\n");
 	printf("signed, void, default, goto, sizeof, volatile, do, if, static, while\n");
-		
+	
+	printf("Tipos de Variaveis:\n");
+	printf("short = 2 bytes = -32.768 a 32.767\n");
+	printf("int = 4 bytes = -2.147.483.648 a 2.147.483.647\n");
+	printf("long = 8 bytes = 2^63 a 2^63-1\n");
+	printf("float = 4 bytes = 1,2 x 10^-38 a 3,4 x 10^38\n");
+	printf("doable = 8 bytes = 2,2 x 10^-308 a 1,8 x 10^308\n");
+	printf("char = 1 byte = 0 a 255\n");
+
 	return 0;
 }
