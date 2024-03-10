498193115c0ddc97afa3df6aacf452fe39a37cb7
Jonathanbez <jbbarroz@gmail.com>
2024-03-09 22:52:16 -0300

    studies C, course of Felipe Louza

---
+++ curso_c_Felipe_Louza/tips_variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	//nomes de variaveis devem comecar com letra (maiuscula ou miniscula) ou _
+	//não podem comecar com numero
+	//não podem conter simbolos { ( + - * /\ ; . , ?
+	int i = 1, j = 2, k;
+	float _variabre;
+	int valor, media, soma;
+	printf("Palavras reservadas em C:\n auto, double, int, struct, break, enum,
+	register, typedef, char, extern, return, union, const, float, short, unsigned,
+	continue, for, signed, void, default, goto, sizeof, volatile, do, if, static, while\n");
+		
+	return 0;
+}
