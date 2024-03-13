d2019f89055cbc17c4ab3a141e9f465bd80007ea
Jonathanbez <jbbarroz@gmail.com>
2024-03-13 07:37:02 -0300

    studies C, course of Felipe Louza, Precedence

--- curso_c_Felipe_Louza/precedence.c
+++ curso_c_Felipe_Louza/precedence.c
@@ -2,14 +2,17 @@
 
 int main ()
 {
-    int x, y, z
+    int x, y, z;
 
-    x = 2
-    y = 3
-    z = 4
-
+    x = 2;
+    y = 3;
+    z = 4;
+    int n_p_m = 0;
+    int p_m = 0;
     //Em C operadores matematicos seguem a mesma regra da Algebra
 
-    p_m = + y * z 
-    printf("Result of first equation (multiplication precedence) %d", p_m)
+    n_p_m = x + y * z;
+    printf("Result of first equation (no multiplication precedence) %d", n_p_m);
+    p_m = (x + y) * z;
+    printf("Result of secundo equation (on multiplication precedence) %d", p_m);
 }
