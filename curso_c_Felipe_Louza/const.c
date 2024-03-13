ffae33114fe6534834a4ee091bd6419d056713f2
Jonathanbez <jbbarroz@gmail.com>
2024-03-13 07:24:54 -0300

    studies C, course of Felipe Louza

--- curso_c_Felipe_Louza/const.c
+++ curso_c_Felipe_Louza/const.c
@@ -19,3 +19,4 @@ int main() {
   printf("%d\n", a);
   printf(hello);
 }
+
---
+++ curso_c_Felipe_Louza/precedence.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x, y, z
+
+    x = 2
+    y = 3
+    z = 4
+
+    //Em C operadores matematicos seguem a mesma
+
+}
