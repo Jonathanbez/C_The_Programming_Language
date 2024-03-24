0c45de0af8e108453741e9e56d8765bea6ee9332
Jonathanbez <jbbarroz@gmail.com>
2024-03-24 20:31:27 -0300

    studies C, course of Felipe Louza, Precedence

---
+++ curso_c_Felipe_Louza/++&--.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//INCREMENT AND DECREMENT IN C
+// a = a + 1 | a++ | ++a
+// a++ is first used in the expression, and then incremented
+// ++a the value is incremented, and then used in the expression
+
+int main()
+{
+    int a
+    a++
+    printf("Value of a is: %d", a);
+}
