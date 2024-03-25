62297e74c5ecd12e297a152d3758932b9dacf8ad
Jonathanbez <jbbarroz@gmail.com>
2024-03-25 07:34:14 -0300

    studies C, course of Felipe Louza, Precedence

--- curso_c_Felipe_Louza/format_specifiers.c
+++ curso_c_Felipe_Louza/format_specifiers.c
@@ -6,6 +6,10 @@
 int main()
 {
     int x = 10, y = 30;
-    char c = "X"
-    printf("%d%d%c\n", x,y,c);
+    char c = 'X';
+    double z = 2.2;
+    printf("%d %d %c\n", x,y,c);
+    // syntax follows the order of the specifier with the variables informed
+    printf("%d %s %f = %d", x, "+", z, x+z);
+    //return 0;
 }
