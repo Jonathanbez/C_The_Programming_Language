e7fa0378e11cb659a28c4f785a3047258f41e4e7
Jonathanbez <jbbarroz@gmail.com>
2024-03-25 07:05:28 -0300

    studies C, course of Felipe Louza, Precedence

---
+++ curso_c_Felipe_Louza/format_specifiers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Format Specifiers for tell the compiler about the type of data
+//in operations of input and output
+
+int main()
+{
+    int x = 10, y = 30;
+    char c = "X"
+    printf("%d%d%c\n", x,y,c);
+}
