e197dc2e32770c0753322a8efbd6aa4e2cb56ede
Jonathanbez <jbbarroz@gmail.com>
2024-03-27 07:56:46 -0300

    studies C, course of Felipe Louza, Precedence

--- curso_c_Felipe_Louza/format_specifiers.c
+++ curso_c_Felipe_Louza/format_specifiers.c
@@ -14,5 +14,6 @@ int main()
     float pi = 3.141592653;
     printf("Pi com 9 casas decimais: %f\n", pi);
     printf("Pi simplificado somente com duas casas decimais: %.2f\n", pi);
+    printf("Emita um ruido\a\n");
 
 }
