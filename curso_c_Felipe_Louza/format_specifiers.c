bdf8a2dce9599c772bce13600069262e4d67a7b3
Jonathanbez <jbbarroz@gmail.com>
2024-03-28 07:55:11 -0300

    studies C, course of Felipe Louza / Format specifier

--- curso_c_Felipe_Louza/format_specifiers.c
+++ curso_c_Felipe_Louza/format_specifiers.c
@@ -14,6 +14,15 @@ int main()
     float pi = 3.141592653;
     printf("PI with 9 decimal places %f\n", pi);
     printf("Simplified PI with only two decimal places: %.2f\n", pi);
+    
+    printf("List of Formart Specifiers:\n");
+    printf("%%d read a Int\n");
+    printf("%%ld read a Long Int\n");
+    printf("%%f read a Float\n");
+    printf("%%ls read a Double\n");
+    printf("%%c read a Char\n");
+    printf("%%s read String\n");
+
     printf("\aEnter the first value: ");
     int f_value;
     scanf("%d", &f_value);
@@ -21,4 +30,6 @@ int main()
     int s_value;
     scanf("%d", &s_value);
     printf("The values entered are: %d e %d\n", f_value, s_value);
+
+    return 0;
 }
