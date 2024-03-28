b7f297e0213718078fa68b1495217c13d889afb3
Jonathanbez <jbbarroz@gmail.com>
2024-03-28 07:36:28 -0300

    studies C, course of Felipe Louza

--- curso_c_Felipe_Louza/format_specifiers.c
+++ curso_c_Felipe_Louza/format_specifiers.c
@@ -12,11 +12,13 @@ int main()
     // syntax follows the order of the specifier with the variables informed
     printf("%d %s %f %f\n", x, "+", z, x+z);
     float pi = 3.141592653;
-    printf("Pi com 9 casas decimais: %f\n", pi);
-    printf("Pi simplificado somente com duas casas decimais: %.2f\n", pi);
-    printf("Emita um ruido\a\n");
-    int f_value, s_value
-    scanf("%d %d", &f_value, &s_value);
-    printf("Digite os valores")
-
+    printf("PI with 9 decimal places %f\n", pi);
+    printf("Simplified PI with only two decimal places: %.2f\n", pi);
+    printf("\aEnter the first value: ");
+    int f_value;
+    scanf("%d", &f_value);
+    printf("Enter the second value: ");
+    int s_value;
+    scanf("%d", &s_value);
+    printf("The values entered are: %d e %d\n", f_value, s_value);
 }
