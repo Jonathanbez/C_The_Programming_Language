e6c3a9d00ae8d778633a32198f6d4adcbd2b8348
Jonathanbez <jbbarroz@gmail.com>
2024-04-29 22:13:00 -0300

    exercises - 1st: linear search

--- Exercises/linear_search.c
+++ Exercises/linear_search.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 int search(int arr[], int N, int x)
 {
@@ -11,4 +11,12 @@ int search(int arr[], int N, int x)
 int main(void)
 {
 	int arr[] = { 2, 3, 4, 10, 40, 42, 50};
-	int x = 10; 
+	int x = 10;
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	int result = search(arr, N, x);
+	(result == -1)
+		? printf("Element is not present in array")
+		: printf("Element is present at index %d", result);
+	return 0;
+}
