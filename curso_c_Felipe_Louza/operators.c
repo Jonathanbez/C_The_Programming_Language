ca4aeb60e397632665205887b07b286ca5e1f51c
Jonathanbez <jbbarroz@gmail.com>
2024-05-08 07:28:41 -0300

    C Couse Felipe Souza - aula 10. and read Geek4Geeks

---
+++ curso_c_Felipe_Louza/operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    int y = 2;
+
+    printf("%d\n", x == y + 8);
+    return 0;
+}
+// output: 0 false / 1 true
+
+void logi
+{
+    // Relational Operators in C
+    printf("&& Conjun")
+
+}
