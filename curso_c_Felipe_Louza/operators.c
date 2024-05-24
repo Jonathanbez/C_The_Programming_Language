760584f4764452d673aa1083926229ca20be70d2
Jonathanbez <jbbarroz@gmail.com>
2024-05-24 04:46:44 -0300

    Felipe Louza | Aula 10 | operadores

--- curso_c_Felipe_Louza/operators.c
+++ curso_c_Felipe_Louza/operators.c
@@ -46,11 +46,13 @@ void lgc_op_not()
 	printf("Logical NOT(!)\n");
         printf("X | !X\n");
 	printf("0 = 1\n");
-	printf("1 = 0\n");
+	printf("1 = 0\n\n");
 }
 
 void example(int x, int y)
 {
-	printf("NOT: X && Y = %d\n", (x != 1 && y != 20));
-	printf("X == Y + 8 = %d\n", x == y + 8);
+	printf("NOT: X && Y = output: %d\n", (x != 1 && y != 20));
+	printf("X == Y + 8 output: %d\n", x == y + 8);
+	printf("a = 0.1 < 0.3 < 0.5 \noutput: false \nhuman logic: true\n");
+	printf("in code C (0.1 < 0.3) && (0.3 < 0.5) output:%d", (0.1 < 0.3) && (0.3 < 0.5));
 }
